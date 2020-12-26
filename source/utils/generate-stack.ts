b6e7ac3e92b055044c813f506f106f6ac1b50195
Sindre Sorhus <sindresorhus@gmail.com>
2020-12-26 17:23:44 +0700

    Update generate-stack.ts

--- source/utils/generate-stack.ts
+++ source/utils/generate-stack.ts
@@ -1,16 +1,15 @@
 import isNode from './node/is-node';
 
 /**
-Generates a useful stacktrace that points to the user's code where the error happened,
-on platforms without the `Error.captureStackTrace` function present
+Generates a useful stacktrace that points to the user's code where the error happened on platforms without the `Error.captureStackTrace()` method.
 
 @hidden
 */
 export const generateStackTrace = () => {
 	let stack = new RangeError('INTERNAL_OW_ERROR').stack!;
 
-	// If we're running in a node environment without the `Error.captureStackTrace` function present
-	// we slice this function from the stack
+	// If we're running in a Node.js environment without the `Error.captureStackTrace` function present
+	// we slice this function from the stack.
 	if (isNode) {
 		stack = stack.split('\n').slice(2).join('\n');
 	}
