3e7415e5ecbc6bb30e5ef843b848ebe4c6fe4978
Vlad Frangu <kingdgrizzle@gmail.com>
2020-12-26 19:19:52 +0200

    chore: remove node check in fallback stack trace util

--- source/utils/generate-stack.ts
+++ source/utils/generate-stack.ts
@@ -1,5 +1,3 @@
-import isNode from './node/is-node';
-
 /**
 Generates a useful stacktrace that points to the user's code where the error happened on platforms without the `Error.captureStackTrace()` method.
 
@@ -7,12 +5,6 @@ Generates a useful stacktrace that points to the user's code where the error hap
 */
 export const generateStackTrace = () => {
 	let stack = new RangeError('INTERNAL_OW_ERROR').stack!;
-
-	// If we're running in a Node.js environment without the `Error.captureStackTrace` function present
-	// we slice this function from the stack.
-	if (isNode) {
-		stack = stack.split('\n').slice(2).join('\n');
-	}
 
 	return stack;
 };
